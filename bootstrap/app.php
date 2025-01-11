5cb83279ee25aa4f6284cd6c58741837ae2ea8fa
Tariq Ayman <tariq.ayman94@gmail.com>
2025-01-11 22:14:10 +0200

    Enhancement

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
                 ->prefix('api')
                 ->name('api.')
                 ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
         },
-        web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
         health: '/up'
     )
     ->withMiddleware(function (Middleware $middleware) {
